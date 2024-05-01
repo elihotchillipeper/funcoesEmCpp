2762a35beea2c5d6527bb1417a989378d0a247fb
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-05-01 00:48:44 +0000

    funções em c++

---
+++ atividadeFuncoes01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int somar(int a, int b){
+    return a + b;
+}
+
+int main(){
+
+    int num1, num2;
+
+    cout << "Digite dois números inteiros para soma-los: ";
+    cin >> num1 >> num2;
+
+    cout << "A soma dos números é " << somar(num1, num2) <<endl;
+
+    return 0;
+}
